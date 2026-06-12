c585e5f7aadec50090be2d27530cbd1719f04aa4
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-12 11:17:17 +0200

    feat: add FastAPI to the project, see if it works

--- automenu-api/main.py
+++ automenu-api/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from automenu-api!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello world!"}
