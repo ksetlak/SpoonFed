cd39e553e2565dc63c11aeb57be089887de8ad5a
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-16 13:18:00 +0200

    feat: data model for a meal

---
+++ automenu-api/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Float, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Meal(Base):
+    __tablename__ = "meals"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    recipe: Mapped[str] = mapped_column(String(2000))
+    calories: Mapped[int] = mapped_column(Integer)
+    carbs: Mapped[float] = mapped_column(Float)
+    fat: Mapped[float] = mapped_column(Float)
+    protein: Mapped[float] = mapped_column(Float)
+
+    def __repr__(self) -> str:
+        return f"Meal (id={self.id!r}, name={self.name!r})"
