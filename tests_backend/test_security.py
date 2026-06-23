d2f71655f8366261363ae9b341d05d5dd35f8ffe
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-23 22:47:43 +0200

    test: unit tests for security module in api

---
+++ tests_backend/test_security.py
@@ -0,0 +1,12 @@
+from automenu_api.security import hash_and_salt_password, verify_password
+
+password = "super-secret-password"
+
+
+def test_hash_and_salt_password():
+    assert not (hash_and_salt_password(password) == hash_and_salt_password(password))
+
+
+def test_verify_password():
+    hash, salt = hash_and_salt_password(password)
+    assert verify_password(hash, salt, password)
