2b7088dc9ee170a5486b02d62e8c700175e10851
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-23 22:34:28 +0200

    chore: rename automenu-api to automenu_api so that imports work

--- automenu_api/security.py
+++ automenu_api/security.py
@@ -8,7 +8,7 @@ def hash_and_salt_password(plain_password: str) -> tuple[str, str]:
     return hash.decode("utf-8"), salt.decode("utf-8")
 
 
-def verify_password(plain_password: str, salt: str, hashed_password: str) -> bool:
+def verify_password(hashed_password: str, salt: str, plain_password: str) -> bool:
     passwd_bytes = plain_password.encode("utf-8")
     salt_bytes = salt.encode("utf-8")
     hash = bcrypt.hashpw(passwd_bytes, salt_bytes).decode("utf-8")
@@ -17,8 +17,3 @@ def verify_password(plain_password: str, salt: str, hashed_password: str) -> boo
 
 # def create_access_token(data: dict, expires_delta: timedelta) -> str:
 # from datetime import timedelta
-# TODO: Add tests
-# password="super-secret-password"
-
-# hash_password(password) == hash_password(password) # False
-# verify_password(hash_password(password), password) # True
