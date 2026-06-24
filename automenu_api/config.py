4ec6b0fa4f66a73ebdc9a8310aba5d5720fba9e9
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-24 16:12:46 +0200

    refactor: move to dotenv and pydantic-settings

---
+++ automenu_api/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    database_url: str
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    debug: bool = False
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
