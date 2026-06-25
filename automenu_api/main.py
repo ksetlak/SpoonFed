eab645acb26319a98b481b14d4d1fee8f46b2cab
Krzysiek Setlak <bigfoot19@wp.pl>
2026-06-25 11:36:41 +0200

    feat: create, update, delete endpoints now gated behind admin access

--- automenu_api/main.py
+++ automenu_api/main.py
@@ -1,14 +1,17 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Form, HTTPException, status
+from fastapi import Depends, FastAPI, Form, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer
+from jwt import PyJWTError, decode  # TODO: Potentially clean up
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from config import settings
 from db import connect_db
 from models import Meal, User
-from security import create_access_token, verify_password
+from security import create_access_token, validate_access_token, verify_password
 
 
 class MealBase(BaseModel):
@@ -29,6 +32,23 @@ class MealCreateModel(MealBase):
 
 class MealReadModel(MealBase):
     id: int
+
+
+# TODO: Potentially move to security, unless we replace auth completely before that happens.
+oauth2_scheme = HTTPBearer()
+
+
+async def verify_admin_access(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        print(f"token: {token}")
+        token_bytes = token.credentials.encode("utf-8")
+        decoded_token = decode(token_bytes, settings.jwt_secret_key, settings.jwt_algorithm)
+    except PyJWTError as e:
+        print(f"DEBUG: PyJWT validation failed with error: {repr(e)}")
+        raise HTTPException(status_code=401, detail="Could not validate credentials") from None
+    if validate_access_token(token_bytes, decoded_token["sub"]):
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    return int(decoded_token["sub"]) == 1
 
 
 app = FastAPI()
@@ -64,7 +84,9 @@ async def authenticate(username: Annotated[str, Form()], password: Annotated[str
 
 
 @app.post("/api/meals", response_model=MealReadModel, status_code=status.HTTP_201_CREATED)
-async def create_meal(meal_in: MealCreateModel):
+async def create_meal(meal_in: MealCreateModel, admin_authorization: Annotated[bool, Depends(verify_admin_access)]):
+    if not admin_authorization:
+        raise HTTPException(status_code=403, detail="Insufficient permissions.")
     async with AsyncSession(engine) as session:
         meal = Meal(**meal_in.model_dump())
         session.add(meal)
@@ -83,8 +105,11 @@ async def retrieve_meal(meal_id: int):
             return MealReadModel.model_validate(meal)
 
 
+# TODO: meal_id is passed in URL, then id from body is uses. Analyze & fix.
 @app.patch("/api/meals/{meal_id}", response_model=MealReadModel, status_code=status.HTTP_200_OK)
-async def update_meal(updated_meal: MealReadModel):
+async def update_meal(updated_meal: MealReadModel, admin_authorization: Annotated[bool, Depends(verify_admin_access)]):
+    if not admin_authorization:
+        raise HTTPException(status_code=403, detail="Insufficient permissions.")
     async with AsyncSession(engine) as session:
         meal = await session.get(Meal, updated_meal.id)
         if not meal:
@@ -99,7 +124,9 @@ async def update_meal(updated_meal: MealReadModel):
 
 
 @app.delete("/api/meals/{meal_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_meal(meal_id: int):
+async def delete_meal(meal_id: int, admin_authorization: Annotated[bool, Depends(verify_admin_access)]):
+    if not admin_authorization:
+        raise HTTPException(status_code=403, detail="Insufficient permissions.")
     async with AsyncSession(engine) as session:
         meal = await session.get(Meal, meal_id)
         if not meal:
